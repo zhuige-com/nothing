0b14bdd6964734987394dffabfef8c9f6d951f02
longwenjunjie <longwenjunj@163.com>
2023-07-14 18:49:06 +0800

    init

---
+++ nothing/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Nothing</title>
+
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+
+        html,
+        body {
+            height: 100%;
+            background: #F2F2F2;
+        }
+
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: #F2F2F2;
+            height: 100%;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        Nothing
+    </div>
+</body>
+
+</html>
